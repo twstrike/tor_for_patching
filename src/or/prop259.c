7c834d0a63a941c13e60f95334c3404cf3ddf5ea
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-03-28 10:30:27 -0500

    Fix guard selection cleanup

--- src/or/prop259.c
+++ src/or/prop259.c
@@ -594,12 +594,9 @@ choose_entry_guard_algo_end(guard_selection_t *guard_selection,
         smartlist_add(guard_selection->used_guards, (void*) guard);
         smartlist_add(used_guards, (void*) guard);
         //XXX this is not correct, save used_guards to state file instead of global variable
+
     if (entry_guard_selection)
-        smartlist_free(entry_guard_selection->primary_guards);
-        smartlist_free(entry_guard_selection->remaining_utopic_guards);
-        smartlist_free(entry_guard_selection->remaining_dystopic_guards);
-        smartlist_free(entry_guard_selection->used_guards);
-        tor_free(entry_guard_selection);
+        guard_selection_free(guard_selection);
 }
 
 //These functions adapt our proposal to current tor code
